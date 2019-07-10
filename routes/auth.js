347783bf9dcdd9625cfb3fa4335723691dc2c1d4
tcb1978 <mattheweldredge@gmail.com>
2019-07-09 17:19:26 -0700

    add contact route

--- routes/auth.js
+++ routes/auth.js
@@ -17,7 +17,7 @@ router.get(`/`, auth, async (req, res) => {
     const user = await User.findById(req.user.id).select('-password');
     res.json(user);
   } catch (error) {
-    console.log(error.message);
+    console.error(error.message);
     res.status(500).send('Server Error');
   }
 });
--- routes/contacts.js
+++ routes/contacts.js
@@ -14,7 +14,7 @@ router.get(`/`, auth, async (req, res) => {
     const contacts = await Contact.find({ user: req.user.id }).sort({ date: -1 });
     res.json(contacts);
   } catch (error) {
-    console.log(error.message);
+    console.error(error.message);
     res.status(500).send('Server Error');
   }
 });
@@ -22,9 +22,42 @@ router.get(`/`, auth, async (req, res) => {
 // @route    POST api/contacts
 // @desc     All a new contact
 // @access    Private
-router.post(`/`, (req, res) => {
-  res.send(`Add contact`);
-});
+router.post(
+  `/`,
+  [
+    auth,
+    [
+      check('name', 'Name is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, phone, type } = req.body;
+
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id
+      });
+
+      const contact = await newContact.save();
+
+      res.json(contact);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 // @route    PUT api/contacts:id
 // @desc     Update contact
