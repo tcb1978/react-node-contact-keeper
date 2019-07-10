3dff526f754dc72e1dcb9f35726825fbb233b82c
tcb1978 <mattheweldredge@gmail.com>
2019-07-09 17:40:59 -0700

    update user route

--- routes/contacts.js
+++ routes/contacts.js
@@ -62,8 +62,34 @@ router.post(
 // @route    PUT api/contacts:id
 // @desc     Update contact
 // @access    Private
-router.put(`/:id`, (req, res) => {
-  res.send(`Update contact`);
+router.put(`/:id`, auth, async (req, res) => {
+  const { name, email, phone, type } = req.body;
+
+  // Build a contact object
+  const contactFields = {};
+  if (name) contactFields.name = name;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.phone = phone;
+  if (type) contactFields.type = type;
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+
+    if (!contact) return res.status(400).json({ msg: 'Contact not found' });
+
+    // Make sure user owns contact
+
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not authorized' });
+    }
+
+    contact = await Contact.findOneAndUpdate(req.params.id, { $set: contactFields }, { new: true });
+
+    res.json(contact);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route    DELETE api/contacts:id
