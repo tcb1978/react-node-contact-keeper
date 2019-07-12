b5d9b59eacf8e98f846198dd517134cf92d992f9
tcb1978 <mattheweldredge@gmail.com>
2019-07-12 14:30:23 -0700

    contact filter form and state

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -5,13 +5,17 @@ import ContactContext from '../../context/contact/contactContext';
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
 
-  const { contacts } = contactContext;
+  const { contacts, filtered } = contactContext;
+
+  if (contacts.length === 0) {
+    return <h4>Please add a contact</h4>;
+  }
 
   return (
     <Fragment>
-      {contacts.map(contact => (
-        <ContactItem contact={contact} key={contact.id} />
-      ))}
+      {filtered !== null
+        ? filtered.map(contact => <ContactItem contact={contact} key={contact.id} />)
+        : contacts.map(contact => <ContactItem contact={contact} key={contact.id} />)}
     </Fragment>
   );
 };
