b8292de41ec961b764160405cb2cd7f16a734301
tcb1978 <mattheweldredge@gmail.com>
2019-07-23 14:21:27 -0700

    alert state & component

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import AlertContext from '../../context/alert/alertContext';
 
 const Register = () => {
+  const alertContext = useContext(AlertContext);
+
+  const { setAlert } = alertContext;
+
   const [user, setUser] = useState({
     name: '',
     email: '',
@@ -18,37 +23,45 @@ const Register = () => {
 
   const onSubmit = e => {
     e.preventDefault();
-    console.log('Register submit');
+    if (name === '' || email === '' || password === '') {
+      setAlert('Please enter all fields', 'danger');
+    } else if (password !== password2) {
+      setAlert('Passwords do not match', 'danger');
+    } else {
+      console.log('Register submit');
+    }
   };
 
   return (
-    <div classname="form-container">
+    <div className="form-container">
       <h1>
         Account <span className="text-primary">Register</span>
       </h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-group">
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" value={name} onChange={onChange} />
+          <input type="text" name="name" value={name} onChange={onChange} required />
         </div>
         <div className="form-group">
           <label htmlFor="email">Email Address</label>
-          <input type="text" email="name" value={email} onChange={onChange} />
+          <input type="text" email="name" value={email} onChange={onChange} required />
         </div>
         <div className="form-group">
           <label htmlFor="password">Password</label>
-          <input type="text" email="name" value={password} onChange={onChange} />
+          <input
+            type="text"
+            email="name"
+            value={password}
+            onChange={onChange}
+            required
+            minLength="6"
+          />
         </div>
         <div className="form-group">
           <label htmlFor="password2">Confirm Password</label>
-          <input type="text" email="name" value={password2} onChange={onChange} />
+          <input type="text" email="name" value={password2} onChange={onChange} required />
         </div>
-        <input
-          type="submit"
-          value="register"
-          className="btn btn-primary btn-block"
-          onSubmit={onSubmit}
-        />
+        <input type="submit" value="register" className="btn btn-primary btn-block" minLength="6" />
       </form>
     </div>
   );
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,13 +1,21 @@
 import React, { useReducer } from 'react';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
+
 import {
+  // eslint-disable-next-line
   REGEISTER_SUCCESS,
+  // eslint-disable-next-line
   REGISTER_FAIL,
+  // eslint-disable-next-line
   USER_LOADED,
+  // eslint-disable-next-line
   AUTH_ERROR,
+  // eslint-disable-next-line
   LOGIN_FAIL,
+  // eslint-disable-next-line
   LOGOUT,
+  // eslint-disable-next-line
   CLEAR_ERRORS
 } from '../types';
 
@@ -18,7 +26,7 @@ const AuthState = props => {
     loading: true,
     error: null
   };
-
+  // eslint-disable-next-line
   const [state, dispatch] = useReducer(authReducer, initialState);
 
   // Load User
